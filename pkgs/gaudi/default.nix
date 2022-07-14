61813b398fb4f30c927b3acd14c4668ab22537ca
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2022-07-14 14:44:57 -0400

    gaudi: various build fixes for linux and darwin

--- pkgs/gaudi/default.nix
+++ pkgs/gaudi/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchFromGitLab
+, fetchFromGitHub
 , fetchpatch
 , boost
 , cmake
@@ -9,6 +10,7 @@
 , fmt
 , gperftools
 , jemalloc
+, libuuid
 , microsoft_gsl
 , python3
 , range-v3
@@ -22,6 +24,22 @@
 let
 
   _boost = boost.override { python = python3; enablePython = true; };
+  _range-v3 =
+    if stdenv.isDarwin && (lib.versionOlder range-v3.version "0.12.0") then
+      # https://github.com/ericniebler/range-v3/pull/1635
+      # https://github.com/NixOS/nixpkgs/pull/181298
+      range-v3.overrideAttrs (prev: rec {
+        version = "0.12.0";
+        src = fetchFromGitHub {
+          owner = "ericniebler";
+          repo = "range-v3";
+          rev = version;
+          hash = "sha256-bRSX91+ROqG1C3nB9HSQaKgLzOHEFy9mrD2WW3PRBWU=";
+        };
+        patches = [];
+      })
+    else
+      range-v3;
 
 in
 
@@ -45,33 +63,48 @@ stdenv.mkDerivation rec {
       includes = [ "GaudiPluginService/include/Gaudi/PluginServiceV2.h" ];
       hash = "sha256-o05iYMCr+RKbkY/DDE0B3U5kVuqYqd4MQeOyVu01JJo=";
     })
+
+    ./cxx20_result_of_fix.patch
   ];
 
   nativeBuildInputs = [
     cmake
     python3.pkgs.six
   ];
+  propagatedBuildInputs = [
+    _boost
+    _range-v3
+    fmt
+    gperftools
+    jemalloc
+    libuuid
+    microsoft_gsl
+    tbb
+    xercesc
+  ] ++ lib.optionals stdenv.isDarwin [ Foundation ];
   buildInputs = [
     _boost
     cppunit
     doxygen
-    fmt
-    gperftools
-    jemalloc
-    microsoft_gsl
     python3
-    range-v3
     root
-    tbb
-    xercesc
     zlib
-  ] ++ lib.optionals stdenv.isDarwin [ Foundation ];
+  ];
+
 
   postPatch = ''
     patchShebangs --build GaudiKernel/scripts/
 
     sed -i GaudiHive/src/PRGraph/PrecedenceRulesGraph.cpp \
       -e '1i#include <boost/filesystem/fstream.hpp>'
+
+    # error: template-id not allowed for destructor
+    substituteInPlace GaudiKernel/src/Lib/KeyedObjectManager.cpp \
+      --replace "::~KeyedObjectManager<T>()" "::~KeyedObjectManager()"
+  '' + lib.optionalString stdenv.isDarwin ''
+    substituteInPlace GaudiKernel/include/GaudiKernel/VectorMap.h \
+      --replace "typename ALLOCATOR::size_type" std::size_t \
+      --replace "typename ALLOCATOR::difference_type" std::ptrdiff_t
   '';
 
   cmakeFlags = [
