b600200ac1cea7a27c2b9b229ef1be5c89985843
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2022-09-14 18:01:41 -0400

    ip6: unstable-2022-08-24 -> unstable-2022-09-03

--- pkgs/ip6/default.nix
+++ pkgs/ip6/default.nix
@@ -9,19 +9,14 @@
 
 stdenv.mkDerivation rec {
   pname = "ip6";
-  version = "unstable-2022-08-24";
+  version = "unstable-2022-09-03";
 
   src = fetchFromGitHub {
     owner = "eic";
     repo = pname;
-    rev = "7d8d142cd701f6fc88f62b92fb9684c8b999ef5a";
-    hash = "sha256-xfB/MKntQXoDWXi9NkqQ7GTrU42AY98vQHLcu6fo/Lc=";
+    rev = "533d83a4e7c993b9a3b729ba20c89a7b01c41e27";
+    hash = "sha256-w8c/3HNFj+8mIlPDsK3Q5A/hJoMtzqwzpBJsa7JgwJc=";
   };
-
-  postPatch = lib.optionalString stdenv.isDarwin ''
-    substituteInPlace templates/setup.sh.in \
-      --replace LD_LIBRARY_PATH DYLD_LIBRARY_PATH
-  '';
 
   nativeBuildInputs = [
     cmake
