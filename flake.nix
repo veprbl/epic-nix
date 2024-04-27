e16cda276a720f01177b8c42dfa0f01db1755612
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-04-27 18:22:13 -0400

    build.yml: enable build on aarch64

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
     let
 
       inherit (nixpkgs) lib;
-      supportedSystems = [ "aarch64-linux" "x86_64-linux" "x86_64-darwin" ];
+      supportedSystems = [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
       pkgsFor = system: import nixpkgs {
         inherit system;
