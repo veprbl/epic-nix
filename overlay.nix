b93bdb956c6ea3990bb3266ffab1d151e5323532
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-05-26 19:30:51 -0400

    root: future-proof for new nixpkgs

--- overlay.nix
+++ overlay.nix
@@ -142,7 +142,7 @@ final: prev: with final; {
       "-Dimt=OFF"
     ];
     NIX_LDFLAGS = lib.optionalString (!stdenv.isDarwin) "--version-script,${./pkgs/root/version.map}";
-    preConfigure = builtins.replaceStrings [ "rm -rf builtins/*" ] [ "" ] prev.preConfigure;
+    preConfigure = builtins.replaceStrings [ "rm -rf builtins/*" "for path in builtins/*; do" ] [ "" "for path in ; do" ] prev.preConfigure;
     buildInputs  = prev.buildInputs ++ [
       openssl
       pythia6
