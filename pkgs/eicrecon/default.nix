90cfaf234f2b71fc7e13f83ddf9e119253ba966c
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-10-08 19:42:48 -0400

    eicrecon: 1.17.0 -> 1.18.0

--- pkgs/eicrecon/default.nix
+++ pkgs/eicrecon/default.nix
@@ -26,7 +26,7 @@
 
 stdenv.mkDerivation rec {
   pname = "EICrecon";
-  version = "1.17.0.${eicrecon-src.shortRev or "dirty"}";
+  version = "1.18.0.${eicrecon-src.shortRev or "dirty"}";
 
   src = eicrecon-src;
 
