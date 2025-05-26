f3404ef36ef6a870ff49c6df03a6720337cdfd2d
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2025-05-26 14:55:34 -0400

    eicrecon: 1.24.2 -> 1.25.0

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
     flake = false;
   };
   inputs.eicrecon-src = {
-    url = "github:eic/EICrecon/v1.24.2";
+    url = "github:eic/EICrecon/v1.25.0";
     flake = false;
   };
   inputs.geant4-src = {
