e96e1db00700183302eed52f8711f00ceb8b5d52
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-02-17 14:50:24 -0500

    dd4hep: 01-33 -> 01-34

--- pkgs/dd4hep/default.nix
+++ pkgs/dd4hep/default.nix
@@ -21,7 +21,7 @@
 
 stdenv.mkDerivation rec {
   pname = "DD4hep";
-  version = "01-33.${dd4hep-src.shortRev or "dirty"}";
+  version = "01-34.${dd4hep-src.shortRev or "dirty"}";
 
   src = dd4hep-src;
 
