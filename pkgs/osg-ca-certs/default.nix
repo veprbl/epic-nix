715b948c68f9e20a1cf2155ed48cf2bd9b39dba3
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-05-04 10:52:11 -0400

    flake.nix: add osg-ca-certs-src

--- pkgs/osg-ca-certs/default.nix
+++ pkgs/osg-ca-certs/default.nix
@@ -1,17 +1,14 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  osg-ca-certs-src,
 }:
 
 stdenv.mkDerivation rec {
+  name = "osg-ca-certs";
   pname = "osg-ca-certs";
-  version = "1.140";
 
-  src = fetchurl {
-    url = "https://repo.opensciencegrid.org/cadist/${version}NEW/osg-certificates-${version}NEW.tar.gz";
-    hash = "sha256-/pc/tss8WQOGxmc+bIJhzJ9wT4c2qW5tNOWhyjw7fXY=";
-  };
+  src = osg-ca-certs-src;
 
   dontConfigure = true;
   dontBuild = true;
