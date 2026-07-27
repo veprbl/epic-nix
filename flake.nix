58aee047b8c6ce55bbe36c4dc9ddaca402e74a86
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-07-27 14:42:44 -0400

    osg-ca-certs: 1.143NEW -> 1.144NEW (#374)

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
     flake = false;
   };
   inputs.osg-ca-certs-src = {
-    url = "https://repo.opensciencegrid.org/cadist/1.143NEW/osg-certificates-1.143NEW.tar.gz";
+    url = "https://repo.opensciencegrid.org/cadist/1.144NEW/osg-certificates-1.144NEW.tar.gz";
     flake = false;
   };
   inputs.podio-src = {
