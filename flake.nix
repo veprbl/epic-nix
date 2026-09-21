087a419a7f38e282eca84b9050a1c803c79ce940
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-09-21 17:15:27 -0400

    dd4hep: 01-36 -> 01-37

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     flake = false;
   };
   inputs.dd4hep-src = {
-    url = "github:AIDASoft/DD4hep/v01-36";
+    url = "github:AIDASoft/DD4hep/v01-37";
     flake = false;
   };
   inputs.edm4eic-src = {
